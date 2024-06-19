2fb64545833b126945ec3b009807f4db4085f80e
Yadhukrishnx <Yadhukrishnx@gmail.com>
2024-06-19 10:08:29 +0530

    Contact Form : Created and Runing with low speed

--- photofolio/settings.py
+++ photofolio/settings.py
@@ -128,3 +128,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # e.g., smtp.gmail.com
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'yadhukrishnx.dev@gmail.com'
+EMAIL_HOST_PASSWORD = 'pkmpxxbrrcfrwfqh'
+DEFAULT_FROM_EMAIL = 'yadhukrishnx.dev@gmail.com'
+CONTACT_EMAIL = 'yadhukrishnx.dev@gmail.com' 
--- viewerapp/views.py
+++ viewerapp/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render,redirect
+from django.urls import reverse_lazy
 from adminapp.models import UserDetails,ProfessionalExperience, Education,Projects,Services
+from django.core.mail import send_mail
+
+from django.conf import settings
+from django.contrib import messages
 
 def index(request):
     return render(request, 'viewerapp/index.html')
@@ -18,6 +23,22 @@ def services(request):
     return render(request, 'viewerapp/services.html',{'services': services})
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        message = message + "Mail sent by " + email + name
+        send_mail(
+            subject,
+            message,
+            email,
+            [settings.CONTACT_EMAIL],
+            fail_silently=False,
+        )
+        messages.success(request, 'Message sent successfully')
+        return redirect('index')
     userdetail = UserDetails.objects.first()
     return render(request, 'viewerapp/contact.html',{'userdetail': userdetail})
 
